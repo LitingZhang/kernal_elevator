63a5bcbc346bc7adcabd04f7119d05739cc3b01d
Liting Zhang <litingzhang0910@gmail.com>
2020-10-13 09:16:30 -0400

    Modified and grouped part1

---
+++ part1/part1.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+int main()
+{
+  sleep(1);
+  sleep(1);
+  sleep(1);
+  sleep(1);
+  sleep(1);
+  sleep(1);
+  sleep(1);
+}
